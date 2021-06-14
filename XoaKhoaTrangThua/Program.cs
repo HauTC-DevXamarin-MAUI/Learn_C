cb4edc584ba53b00fd0155fbf4b57d526380e61b
CongHau <conghau0112@gmail.com>
2021-06-14 17:13:17 +0700

    add XoaKhoangTrangThua && In Xuong Dong

---
+++ XoaKhoaTrangThua/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XoaKhoaTrangThua
+{
+    class Program
+    {
+        // "   hau dep trai   qua di  " =>"hau dep trai qua di"
+        static void Main(string[] args)
+        {
+            string s = " Hau  Dep       Choai     Qua DI   Thoi   ";
+            //Console.WriteLine("Chuoi hien tai la: {0}", s);
+            //XoaKhoangTrangThua(s);
+            InXuongDong(s);
+            Console.ReadLine();
+        }
+        static void XoaKhoangTrangThua(string s)
+        {
+            string s1 = s.Trim();
+            
+            for (int i = 0;i < s1.Length; i++)
+            {
+                if (s1[i].Equals(' '))
+                {
+                    if (s1[i + 1].Equals(' '))
+                    {
+                        s1 = s1.Remove(i,1);
+                        i --;
+                    }
+                }
+            }
+            Console.WriteLine("Chuoi s khi Xoa khoan trang: {0}\n ",s1);
+            int x;
+            
+        }
+        static void InXuongDong(string s)
+        {
+            int x, y, i;
+            x = Console.CursorLeft;
+            y = Console.CursorTop;
+            i = y;
+
+            foreach (char item in s)
+            {
+                Console.SetCursorPosition(x,i);
+                Console.Write(item);
+                if (item.Equals(' '))
+                {
+                    i =y;
+                }
+                else
+                {
+                    i++;
+                }
+                x++;
+            }
+        }
+    }
+}
---
+++ timGiaTriXaNhatPT_X/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace timGiaTriXaNhatPT_X
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int[] a = new int[6];
+            KhoiTaoMang(a);
+            XuatMang(a);
+            
+            while (true)
+            {
+                Console.Write("\nNhap gia tri can tim X: ");
+                 int x = int.Parse(Console.ReadLine());
+                 isX(a,x);
+            }
+
+            Console.ReadLine();
+        }
+        //Mang: 1 2 3 6 9
+        public static void KhoiTaoMang(int[] a)
+        
+        {
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.Write("Moi ban nhap phan tu thu {0}: ", i+1);
+                a[i] = int.Parse(Console.ReadLine());
+            }
+        }
+        public static void XuatMang(int[] a)
+        {
+            Console.WriteLine("Mang: ");
+            foreach (int item in a)
+            {
+                if (item != null)
+                {
+                    Console.Write("{0,4}", item);
+                }
+            }
+        }
+        public static void isX(int []a, int x)
+        {
+            int count = a.Length -1;
+
+            if (!a.Contains(x))
+            {
+                Console.WriteLine("khong Ton Tai X");
+                return;
+            }
+            //0 1 2 3 4 5 (6)
+            // 0 1 2 3 4 (5)
+            else
+            {
+                
+            }
+            
+        }
+        
+    }
+}
